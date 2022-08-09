0396d7e3ae1c7d86a12cbdb45690f8ad322cb50e
gthb2-bin <37474736+ddyachok@users.noreply.github.com>
2022-08-09 21:46:03 +0300

    Initial commit

---
+++ ImpulseDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ImpulseDemo
+//
+//  Created by Daniel Dyachok on 09.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
